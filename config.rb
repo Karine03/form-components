23e2589430139cbd0974fd5889bdac50f980e8b8
Karine Bialobroda <karine.bialobroda@gmail.com>
2016-11-28 11:02:25 +0100

    add forms

--- config.rb
+++ config.rb
@@ -89,3 +89,10 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+helpers do
+
+  def github_sass_url(file)
+    "https://raw.githubusercontent.com/lewagon/ui-components/master/source/stylesheets/components/_#{file}.scss"
+  end
+end
